e84992889c19b5543164d5da5119ba46ebd049fc
Ryan Scarbel <ryanscarbel@gmail.com>
2021-10-04 08:48:52 -0400

    Added all methods to server.js

--- server.js
+++ server.js
@@ -2,22 +2,35 @@ const express = require('express');
 require('dotenv').config();
 const logger = require('morgan');
 const port = process.env.PORT || 3000;
-const pokemon = require('./models/pokemon')
-const caught = require('./models/caught')
+const pokemon = require('./models/pokemon');
+const caught = require('./models/caught');
+const path = require('path');
 
 const app = express();
 const methodOverride = require("method-override")
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
 
+// INDUCES
+/*
+  Index
+  New
+  Delete
+  Update
+  Create
+  Edit
+  Show
+*/
 
 app.get('/pokedex/entry/:pokemonId', (req,res) => {
+  console.log(req.params.pokemonId)
   res.render('entry.ejs', {
-    pokemonData: pokemon[caught[req.params.pokemonID]]
+    pokemonData: pokemon[req.params.pokemonId]
   })
 });
 
@@ -45,6 +58,31 @@ app.get('/pokedex', (req,res) => {
 app.get('*', (req,res) => {
   res.redirect('pokedex')
 });
+
+// Delete Route
+app.delete('/pokedex/:indexOfPokemon', (req, res) => {
+  // This will remove one fruit from the array
+  console.log(req.method)
+  // remove the object from the array
+  caught.splice(req.params.indexOfPokemon - 1, 1)
+  res.redirect('/pokemon')
+})
+
+// Create Route
+app.post('/pokedex/:indexOfPokemon', (req, res) => {
+  let isHighestIndex = true;
+  for (let i = 0;i < caught.length; i++) {
+    if (caught[i] > req.params.indexOfPokemon) {
+      caught.splice(i,0,pokemon[req.params.indexOfPokemon - 1]);
+      isHighestIndex = false
+      break;
+    }
+  }
+  if (isHighestIndex) {
+    caught.push(pokemon[req.params.indexOfPokemon - 1]);
+  }
+  res.redirect("/pokemon")
+})
 
 app.listen(port, () => {
   console.log(`Express is listening on port: ${port}`)
